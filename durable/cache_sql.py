419ee8260c5d2c4b2ab254e5a7e9bdda8f238016
Florian Boucault <florian@boucault.net>
2023-11-17 13:49:55 +0100

    Refactor SQL caching decorator to use a custom ResultStore

--- durable/cache_sql.py
+++ durable/cache_sql.py
@@ -1,8 +1,10 @@
 import functools
-import sqlite3
 import pickle
-import hashlib
-from typing import Callable, Any, Optional, Dict, Tuple
+import sqlite3
+from typing import Callable, Dict, Optional, Tuple
+
+from .durable import ResultStore, caching_decorator, key_for_function_call
+
 
 def sql_cached(connection_string: str,
                create_table_sql: Optional[str] = None, 
@@ -13,6 +15,37 @@ def sql_cached(connection_string: str,
     A decorator that caches the results of a function in a database.
     Allows customization of the SQL statements and the key generation logic.
     """
+
+    class SQLResultStore(ResultStore):
+        def __init__(self, connection_string, create_table_sql, select_sql, insert_sql):
+            self.connection = sqlite3.connect(connection_string)
+            self.create_table_sql = create_table_sql
+            self.select_sql = select_sql
+            self.insert_sql = insert_sql
+
+        def __del__(self):
+            self.connection.close()
+
+        def get_result(self, key):
+            cursor = self.connection.cursor()
+            cursor.execute(self.select_sql, (key,))
+            cached_result = cursor.fetchone()
+
+            if cached_result is not None:
+                # Deserialize and return the cached result
+                return pickle.loads(cached_result[0])
+            else:
+                raise KeyError()
+
+        def store_result(self, key, result):
+            serialized_result = pickle.dumps(result)
+            cursor = self.connection.cursor()
+            cursor.execute(self.insert_sql, (key, serialized_result))
+            self.connection.commit()
+
+        def store_exception(self, key, exception):
+            pass
+
     if create_table_sql is None:
         create_table_sql = """
             CREATE TABLE IF NOT EXISTS cache (
@@ -28,41 +61,7 @@ def sql_cached(connection_string: str,
         insert_sql = "INSERT INTO cache (key, result) VALUES (?, ?)"
 
     if key_func is None:
-        def default_key_func(func: Callable, args: Tuple, kwargs: Dict) -> str:
-            key_data = (func.__name__, args, frozenset(kwargs.items()))
-            return hashlib.md5(pickle.dumps(key_data)).hexdigest()
+        key_func = key_for_function_call
 
-        key_func = default_key_func
-
-    def decorator(func: Callable) -> Callable:
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
-            conn = sqlite3.connect(connection_string)
-            cursor = conn.cursor()
-
-            # Execute the provided CREATE TABLE SQL
-            cursor.execute(create_table_sql)
-
-            # Generate a unique key using the provided key generation function
-            key = key_func(func, args, kwargs)
-
-            # Check if the result is in cache using the provided SELECT SQL
-            cursor.execute(select_sql, (key,))
-            cached_result = cursor.fetchone()
-
-            if cached_result is not None:
-                # Deserialize and return the cached result
-                result = pickle.loads(cached_result[0])
-            else:
-                # Execute the function and cache the result
-                result = func(*args, **kwargs)
-                serialized_result = pickle.dumps(result)
-                cursor.execute(insert_sql, (key, serialized_result))
-                conn.commit()
-
-            conn.close()
-            return result
-
-        return wrapper
-
-    return decorator
+    store = SQLResultStore(connection_string, create_table_sql, select_sql, insert_sql)
+    return functools.partial(caching_decorator, key_func=key_func, store=store)
