06cced46737379245ac1afdd96e59e05c0f2f2e0
Florian Boucault <florian@boucault.net>
2023-11-17 14:04:27 +0100

    moved code

--- durable/cache_sql.py
+++ durable/cache_sql.py
@@ -6,6 +6,37 @@ from typing import Callable, Dict, Optional, Tuple
 from .durable import ResultStore, caching_decorator, key_for_function_call
 
 
+class SQLResultStore(ResultStore):
+    def __init__(self, connection_string, create_table_sql, select_sql, insert_sql):
+        self.connection = sqlite3.connect(connection_string)
+        self.create_table_sql = create_table_sql
+        self.select_sql = select_sql
+        self.insert_sql = insert_sql
+
+    def __del__(self):
+        self.connection.close()
+
+    def get_result(self, key):
+        cursor = self.connection.cursor()
+        cursor.execute(self.select_sql, (key,))
+        cached_result = cursor.fetchone()
+
+        if cached_result is not None:
+            # Deserialize and return the cached result
+            return pickle.loads(cached_result[0])
+        else:
+            raise KeyError()
+
+    def store_result(self, key, result):
+        serialized_result = pickle.dumps(result)
+        cursor = self.connection.cursor()
+        cursor.execute(self.insert_sql, (key, serialized_result))
+        self.connection.commit()
+
+    def store_exception(self, key, exception):
+        pass
+
+
 def sql_cached(connection_string: str,
                create_table_sql: Optional[str] = None, 
                select_sql: Optional[str] = None, 
@@ -15,37 +46,6 @@ def sql_cached(connection_string: str,
     A decorator that caches the results of a function in a database.
     Allows customization of the SQL statements and the key generation logic.
     """
-
-    class SQLResultStore(ResultStore):
-        def __init__(self, connection_string, create_table_sql, select_sql, insert_sql):
-            self.connection = sqlite3.connect(connection_string)
-            self.create_table_sql = create_table_sql
-            self.select_sql = select_sql
-            self.insert_sql = insert_sql
-
-        def __del__(self):
-            self.connection.close()
-
-        def get_result(self, key):
-            cursor = self.connection.cursor()
-            cursor.execute(self.select_sql, (key,))
-            cached_result = cursor.fetchone()
-
-            if cached_result is not None:
-                # Deserialize and return the cached result
-                return pickle.loads(cached_result[0])
-            else:
-                raise KeyError()
-
-        def store_result(self, key, result):
-            serialized_result = pickle.dumps(result)
-            cursor = self.connection.cursor()
-            cursor.execute(self.insert_sql, (key, serialized_result))
-            self.connection.commit()
-
-        def store_exception(self, key, exception):
-            pass
-
     if create_table_sql is None:
         create_table_sql = """
             CREATE TABLE IF NOT EXISTS cache (
