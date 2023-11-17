15a0a17ce84cc3df458f986e5e51d203ac45571b
Florian Boucault <florian@boucault.net>
2023-11-17 14:06:47 +0100

    Update prefix in observe.py

--- observe.py
+++ observe.py
@@ -3,7 +3,7 @@ from durable import durable
 store = durable.get_store(durable.DEFAULT_CACHE_STORE_ID)
 
 # print(store["/my_long_op/http://1"])
-prefix = "/my_long_op/"
+prefix = "/"
 # for k, v in store.items(from_key=prefix):
 for k, v in durable.keys_with_prefix(store, prefix):
     print(k, v)
