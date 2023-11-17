dd718001aee58ebfb3a38ac480723fe5abeecf34
Florian Boucault <florian@boucault.net>
2023-11-17 15:20:08 +0100

    Update result store and import statement in observe.py

--- observe.py
+++ observe.py
@@ -1,9 +1,8 @@
-from durable import durable
+from durable.durable import DictResultStore
 from durable.cache_sql import SQLResultStore
 
 result_store = SQLResultStore("sql_cache.db")
-# result_store = durable.DictResultStore(durable.get_store(durable.DEFAULT_CACHE_STORE_ID))
+# result_store = DictResultStore(durable.get_store(durable.DEFAULT_CACHE_STORE_ID))
 
-# prefix = "/my_long_op/http://1"
 for v in result_store.get_results("is_speaker_in_video"):
     print(v)
