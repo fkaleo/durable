72354aba94c8442bb11d21667fcd503d9d003b9f
Florian Boucault <florian@boucault.net>
2023-11-22 19:08:10 +0100

    Add ray.remote_and_cached decorator

--- durable/ray.py
+++ durable/ray.py
@@ -1,10 +1,25 @@
+import functools
 from functools import wraps
 from typing import Callable
 
+import ray
+
+from .cache_sql import sql_cached
 from .durable import FutureProtocol
+from .future import result_from_future
+from .unique import ensure_unique
 
 
 def submit(func: Callable) -> Callable[..., FutureProtocol]:
+    """
+    Decorator to convert a Ray remote function to return a FutureProtocol object.
+    
+    Args:
+        func (Callable): The Ray remote function to be wrapped.
+
+    Returns:
+        Callable[..., FutureProtocol]: A function that, when called, returns a FutureProtocol object.
+    """
 
     @wraps(func)
     def wrapper(*args, **kwargs) -> FutureProtocol:
@@ -12,5 +27,44 @@ def submit(func: Callable) -> Callable[..., FutureProtocol]:
 
         # note that .future() comes from https://github.com/ray-project/ray/pull/15425
         return object_ref.future()
+
+    return wrapper
+
+
+def remote_and_cached(num_cpus: float, memory: float, cache_db: str, num_gpus: float = 0.0) -> Callable:
+    """
+    A decorator factory for modifying a function to enable remote execution and caching.
+
+    Parameters
+    ----------
+    num_cpus : float
+        Number of CPUs to allocate for the remote function.
+    memory : float
+        Amount of memory (in bytes) to allocate for the remote function.
+    cache_db : str
+        Connection string for the database to use for caching.
+    num_gpus : float, optional
+        Number of GPUs to allocate for the remote function (default is 0.0).
+
+    Returns
+    -------
+    Callable
+        A decorator that, when applied to a function, modifies it for remote execution and caching.
+
+    """
+
+    def wrapper(func):
+
+        @ensure_unique
+        @result_from_future
+        @sql_cached(cache_db)
+        @submit
+        @functools.wraps(func)
+        @ray.remote(num_cpus=num_cpus, memory=memory, num_gpus=num_gpus)
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
 
     return wrapper
---
+++ durable/tests/test_ray.py
@@ -0,0 +1,64 @@
+import pytest
+
+import ray
+
+from ..ray import remote_and_cached, submit
+from ..durable import FutureProtocol
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ray_setup():
+    ray.init()
+    yield
+    ray.shutdown()
+
+
+def test_simple_remote_function():
+
+    @submit
+    @ray.remote
+    def simple_function():
+        return 42
+
+    future = simple_function()
+    assert isinstance(future, FutureProtocol)
+    assert future.result() == 42
+
+def test_function_with_arguments():
+    @submit
+    @ray.remote
+    def add(a, b):
+        return a + b
+
+    future = add(10, 32)
+    assert isinstance(future, FutureProtocol)
+    assert future.result() == 42
+
+def test_error_handling():
+    @submit
+    @ray.remote
+    def failing_function():
+        raise ValueError("Expected error")
+
+    future = failing_function()
+    with pytest.raises(ValueError, match="Expected error"):
+        future.result()
+
+@pytest.fixture
+def temp_sqlite_db_url(tmp_path):
+    db_path = tmp_path / "sql_cache.db"
+    db_url = f"sqlite:///{db_path}"
+    yield db_url
+
+def multiply(x: int) -> int:
+    return x * x
+
+def test_remote_and_cached_basic(temp_sqlite_db_url):
+    multiply_remote = remote_and_cached(num_cpus=0.1, memory=64, cache_db=temp_sqlite_db_url)(multiply)
+
+
+    assert multiply_remote.__name__ == multiply.__name__
+    assert multiply_remote.__doc__ == multiply.__doc__
+
+    result = multiply_remote(4)
+    assert result == 16
