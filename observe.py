c32386f2cb48e970484cd323b68a490f20fe7dda
Florian Boucault <florian@boucault.net>
2025-05-28 09:03:43 +0200

    cleaner test

--- observe.py
+++ observe.py
@@ -1,8 +1,8 @@
-from durable.cache_rocksdb import DictResultStore
+from durable.cache_rocksdb import DictResultStore, get_store, DEFAULT_CACHE_STORE_ID
 from durable.cache_sql import SQLAlchemyResultStore
 
 result_store = SQLAlchemyResultStore("sqlite:///sql_cache.db")
-# result_store = DictResultStore(durable.get_store(durable.DEFAULT_CACHE_STORE_ID))
+# result_store = DictResultStore(get_store(DEFAULT_CACHE_STORE_ID))
 
 for v in result_store.get_function_calls("is_speaker_in_video"):
     print(v)
